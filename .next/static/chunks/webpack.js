2ed5b91f0bd1e8878662a573a1ba3c3dfb36dc96
nicolassartini <nicolasgsartini@gmail.com>
2025-10-12 23:10:44 -0300

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c9334dfe99edb35"; }
+/******/ 		__webpack_require__.h = function() { return "41c1b963fef7600f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
